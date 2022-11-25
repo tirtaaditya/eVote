9ef9b38413349766f6ec5bb104ac8254089c7578
Tirta Aditya Pratama Iswara <tirta.aditya.pratama.iswara@gmail.com>
2022-11-25 14:59:41 +0700

    Fix Home Page

--- app/Views/MasterPageView.php
+++ app/Views/MasterPageView.php
@@ -335,7 +335,7 @@ $user = session('user');
 										<div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
 											<!--begin::Menu wrapper-->
 											<div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-												<img src="<?=base_url()?>/assets/media/avatars/150-26.jpg" alt="user" />
+												<img src="<?=base_url()?>/assets/media/avatars/blank.png" alt="user" />
 											</div>
 											<!--begin::Menu-->
 											<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -344,7 +344,7 @@ $user = session('user');
 													<div class="menu-content d-flex align-items-center px-3">
 														<!--begin::Avatar-->
 														<div class="symbol symbol-50px me-5">
-															<img alt="Logo" src="<?=base_url()?>/assets/media/avatars/150-26.jpg" />
+															<img alt="Logo" src="<?=base_url()?>/assets/media/avatars/blank.png" />
 														</div>
 														<!--end::Avatar-->
 														<!--begin::Username-->
