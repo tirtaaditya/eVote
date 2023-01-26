927d4c4894bef939ce93cba829671130eed40e7e
Tirta Aditya Pratama Iswara <tirta.aditya.pratama.iswara@gmail.com>
2023-01-26 10:36:48 +0700

    Logo Mediatron

--- app/Views/AbsenCreateView.php
+++ app/Views/AbsenCreateView.php
@@ -11,7 +11,7 @@
 			<div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url(<?=base_url()?>/assets/media/illustrations/sketchy-1/14.png">
 				<div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
 					<a href="<?=base_url()?>" class="mb-12">
-						<img alt="Logo" src="<?=base_url()?>/assets/media/logos/logo.jpg" class="h-150px" />
+						<img alt="Logo" src="<?=base_url()?>/assets/media/logos/LogoHeader.png" class="h-150px" />
 					</a>
 					<div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
 						<div class="text-center mb-10">
