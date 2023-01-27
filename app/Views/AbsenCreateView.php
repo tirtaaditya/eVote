d28d8f87b8e09db82900068410db7e9fc95af539
Tirta Aditya Pratama Iswara <tirta.aditya.pratama.iswara@gmail.com>
2023-01-27 11:11:59 +0700

    Update Loading

--- app/Views/AbsenCreateView.php
+++ app/Views/AbsenCreateView.php
@@ -32,15 +32,10 @@
 		<div class="screen">
 			<div class="box">
 				<div class="loader">
-					<div class="spinner-grow text-muted"></div>
-					<div class="spinner-grow text-primary"></div>
-					<div class="spinner-grow text-success"></div>
-					<div class="spinner-grow text-info"></div>
-					<div class="spinner-grow text-warning"></div>
-					<div class="spinner-grow text-danger"></div>
-					<div class="spinner-grow text-secondary"></div>
-					<div class="spinner-grow text-dark"></div>
-					<div class="spinner-grow text-light"></div>
+					<div class="spinner-grow text-danger"></div>&nbsp;&nbsp;
+					<div class="spinner-grow text-muted"></div>&nbsp;&nbsp;
+					<div class="spinner-grow text-primary"></div>&nbsp;&nbsp;
+					<div class="spinner-grow text-success"></div>&nbsp;&nbsp;
 				</div>
 			</div>
 		</div>
