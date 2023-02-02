ba180f29f4283edbf7191f8abf0f45f86f36f51b
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-02 15:31:28 +0700

    Update MasterUserVote

--- app/Models/SubmitFormModel.php
+++ app/Models/SubmitFormModel.php
@@ -8,21 +8,23 @@ class SubmitFormModel extends Model
 	{
 		$this->db->transBegin();
 
-		if(!empty($kuasa))
-		{
-		    $this->db->table('transaction_users_kuasa')->insertBatch($kuasa);
-		}
-
-		if ($this->db->transStatus() === FALSE)
-		{
-		    $this->db->transRollback();
-		    return FALSE;
-		}
-		else
-		{
-		    $this->db->transCommit();
-		    return TRUE;
-		} 
+        $this->db->table('master_users_vote')->where('identity_code', $data['identity_code'])->update($data);
+		
+        if(!empty($kuasa))
+        {
+            $this->db->table('transaction_users_kuasa')->insertBatch($kuasa);
+        }
+        
+        if ($this->db->transStatus() === FALSE)
+        {
+            $this->db->transRollback();
+            return FALSE;
+        }
+        else
+        {
+            $this->db->transCommit();
+            return TRUE;
+        } 
 	}
 
 
