a5c4f183310007f18dee38f0e0ef73e7a6523494
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-02 08:51:31 +0700

    dynamic Vote

--- app/Models/CandidateModel.php
+++ app/Models/CandidateModel.php
@@ -21,5 +21,15 @@ class CandidateModel extends Model
         
         return $result->getFirstRow('array');        
     }
+    
+    function getHasilVote()
+    {
+        $query = "SELECT a.name, a.master_candidate_vote_id, COUNT(b.transaction_voting_id) AS total_suara FROM master_candidate_vote a LEFT JOIN transaction_voting b ON a.master_candidate_vote_id=b.master_candidate_vote_id GROUP BY a.name, a.master_candidate_vote_id";
+       
+        $result = $this->db->query($query);
+        
+        return $result->getResultArray();        
+    }
+
 
 }
