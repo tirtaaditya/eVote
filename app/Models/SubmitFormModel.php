f1d1435095ccd4c2d966b9360fb6f4ca989c7d8c
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-02 15:25:59 +0700

    SaveData

--- app/Models/SubmitFormModel.php
+++ app/Models/SubmitFormModel.php
@@ -8,23 +8,21 @@ class SubmitFormModel extends Model
 	{
 		$this->db->transBegin();
 
-        $this->db->table('master_users_vote')->insert($data);
+		if(!empty($kuasa))
+		{
+		    $this->db->table('transaction_users_kuasa')->insertBatch($kuasa);
+		}
 
-        if(!empty($kuasa))
-        {
-            $this->db->table('transaction_users_kuasa')->insertBatch($kuasa);
-        }
-        
-        if ($this->db->transStatus() === FALSE)
-        {
-            $this->db->transRollback();
-            return FALSE;
-        }
-        else
-        {
-            $this->db->transCommit();
-            return TRUE;
-        } 
+		if ($this->db->transStatus() === FALSE)
+		{
+		    $this->db->transRollback();
+		    return FALSE;
+		}
+		else
+		{
+		    $this->db->transCommit();
+		    return TRUE;
+		} 
 	}
 
 
