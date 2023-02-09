fc5c29c04f51089d0f8693e71528283f3165fadd
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-09 22:04:18 +0700

    Whatsapp Roling

--- app/Helpers/WhatsappHelper.php
+++ app/Helpers/WhatsappHelper.php
@@ -13,7 +13,9 @@ class WhatsappHelper
 
 	function sendWhatsapp($module, $nomorWhatsapp, $message)
 	{
-		$url = "https://api.watsap.id/send-message";
+		$statusWhatsapp = "";
+		
+		$url = "https://api.kirimwa.id/v1/messages";
 
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_URL, $url);
@@ -21,17 +23,17 @@ class WhatsappHelper
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 		$headers = array(
+		   "Authorization: Bearer qtkl44hm/c2FdwgDzxBDKx5NYbs+GUgkVdr55Hd6UJwIJIANexmUTSBByiugRMAg-tirta",
 		   "Content-Type: application/json",
 		);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 		$data = <<<DATA
 		{
-			"id_device" : "4837",
-			"api-key" : "f915c170b6ed54a006a1353a27cd1c3240716f94",
-			"sender" : "6287867551080",
-			"no_hp" : "$nomorWhatsapp",
-			"pesan" : "$message"
+			"phone_number":"$nomorWhatsapp",
+			"message" : "$message",
+			"device_id":"samsungmod",
+			"message_type":"text"
 		}
 		DATA;
 
@@ -42,24 +44,47 @@ class WhatsappHelper
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 		$result = curl_exec($curl);
-		$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		curl_close($curl);
+		$kwid = json_decode($result);
 		
-		if($httpcode == "503")
+		if(empty($kwid->id))
 		{
-			$url =  "https://api.kirimwa.id/v1/messages";
-			$data = array("phone_number" => $nomorWhatsapp, "message" => $message, "device_id" => "samsungmod", "message_type" => "text");
-			$options = array(
-			'http' => array(
-				'method'  => 'POST',
-				'content' => json_encode( $data ),
-				'header'=>  "Content-Type: application/json\r\n" .
-							"Accept: application/json\r\n" .
-					"Authorization: Bearer qtkl44hm/c2FdwgDzxBDKx5NYbs+GUgkVdr55Hd6UJwIJIANexmUTSBByiugRMAg-tirta\r\n"
-				)
+			$url = "https://api.watsap.id/send-message";
+
+			$curl = curl_init($url);
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+			$headers = array(
+			   "Content-Type: application/json",
 			);
-			$context  = stream_context_create( $options );
-			$result = file_get_contents( $url, false, $context );
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+			$data = <<<DATA
+			{
+				"id_device" : "4837",
+				"api-key" : "f915c170b6ed54a006a1353a27cd1c3240716f94",
+				"sender" : "6287867551080",
+				"no_hp" : "$nomorWhatsapp",
+				"pesan" : "$message"
+			}
+			DATA;
+
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+			//for debug only!
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+			$result = curl_exec($curl);
+			$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			curl_close($curl);
+			
+			if($httpcode == 503)
+			{
+				$statusWhatsapp = "Server whatsapp is busy";
+			}
 		}
 				
 		$logWhatsapp['module'] = $module;
@@ -68,6 +93,8 @@ class WhatsappHelper
 		$logWhatsapp['response'] = $result;
 		
 		$this->whatsappModels->insertLogWA($logWhatsapp);
+		
+		return $statusWhatsapp;
 	}
 
 }
