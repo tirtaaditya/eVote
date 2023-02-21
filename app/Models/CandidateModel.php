bb50e26dc32f7a73c06a057005aff0b1fbd0e810
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-21 15:38:46 +0700

    Vote 2

--- app/Models/CandidateModel.php
+++ app/Models/CandidateModel.php
@@ -24,13 +24,22 @@ class CandidateModel extends Model
     
     function getHasilVote()
     {
-        $query = "SELECT a.name, a.master_candidate_vote_id, COUNT(b.transaction_voting_id) AS total_suara FROM master_candidate_vote a LEFT JOIN transaction_voting b ON a.master_candidate_vote_id=b.master_candidate_vote_id GROUP BY a.name, a.master_candidate_vote_id";
+        $query = "SELECT a.name, a.master_candidate_vote_id, COUNT(b.transaction_voting_id) AS total_suara FROM master_candidate_vote a LEFT JOIN transaction_voting b ON a.master_candidate_vote_id=b.master_candidate_vote_id WHERE a.master_vote_id=1 GROUP BY a.name, a.master_candidate_vote_id";
        
         $result = $this->db->query($query);
         
         return $result->getResultArray();        
     }
 
+    function getHasilVote2()
+    {
+        $query = "SELECT a.name, a.master_candidate_vote_id, COUNT(b.transaction_voting_id) AS total_suara FROM master_candidate_vote a LEFT JOIN transaction_voting b ON a.master_candidate_vote_id=b.master_candidate_vote_id WHERE a.master_vote_id=2 GROUP BY a.name, a.master_candidate_vote_id";
+       
+        $result = $this->db->query($query);
+        
+        return $result->getResultArray();        
+    }	
+	
     function updateVoteDate($data)
 	{
 		$this->db->transBegin();
