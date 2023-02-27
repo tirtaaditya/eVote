60900a86557f0f9c33350e8d39064b479c9061b5
tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>
2023-02-27 22:56:12 +0700

    Update HomeView.php
    Signed-off-by: tirtaaditya <tirta.aditya.pratama.iswara@gmail.com>

--- app/Views/HomeView.php
+++ app/Views/HomeView.php
@@ -320,7 +320,7 @@ if($user['role'] !== 'Voters') { ?>
     function saveVote()
     {
         var processVoteUrl = "<?=$processVoteUrl?>";
-	if(document.querySelector('input[name="pengawas"]:checked').value == null)
+	if(document.querySelector('input[name="pengawas"]:checked') == null)
 	{
 		Swal.fire({
 			text: "Pengawas belum dipilih",
